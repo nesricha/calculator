a94e1b53a48fe8a52d93f00a0e4b071a88b060c3
nesricha <nesrine2000@hotmail.it>
2023-11-13 19:51:49 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,117 +1,80 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const calculator = ["C", "<-", "(", ")", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "/", "0", ".", "="]
+  const keybAlternatives = ["Backspace", "Delete", "Enter"]
+  const [exp, setExp] = useState<string[]>([])
+  const [res, setRes] = useState<number | undefined>(0)
+
+  const calcRes = () => {
+    let expression = exp.join("")
+    let result = new Function("return " + expression)
+    exp.length ? setRes(result()) : console.log("Invalid expression")
+  }
+
+  const eventHandler = (key: any) => {
+    try {
+      switch (key) {
+        case "C":
+        case "Delete":
+          setExp([])
+          setRes(0)
+          break;
+
+        case "<-":
+        case "Backspace":
+          setExp(exp.slice(0, -1))
+          break;
+
+        case "=":
+        case "Enter":
+          calcRes()
+          break;
+
+        default:
+          (exp[0] !== "0" && exp[0] !== "undefined") ? setExp([...exp, key]) : setExp([`${key}`])
+          break
+      }
+    } catch (err) {
+      setRes(undefined)
+      console.log(`Invalid expression. More details: ${JSON.stringify({ err }, null)}`)
+    }
+  }
+
+  if (typeof document !== "undefined") {
+    document.addEventListener("keydown", ev => {
+      (calculator.includes(ev.key) || keybAlternatives.includes(ev.key)) && eventHandler(ev.key)
+      ev.stopPropagation()
+    })
+  }
+
+  useEffect(() => {
+    setExp([`${res}`])
+  }, [res])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='bg-yellow-50 py-10 h-[100vh]'>
+      <p className='text-center p-10 text-5xl font-bold mb-8'>CALCULATOR</p>
+      <div className='mx-auto w-4/12 min-w-[300px] max-w-[400px] bg-teal-800 text-white rounded-2xl p-2'>
+        <div className='text-right m-4 mb-6 p-4 bg-zinc-400 rounded-lg border-8 border-b-4 border-teal-900'>
+          <p className='text-xl'>
+            {
+              !exp.length ? `${res}` : `${exp.join("")}`
+            }
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='mx-auto text-center font-bold text-lg'>
+          <div className='grid grid-cols-4 mx-auto'>
+            {calculator.map(key => <a key={key} href="#" className='mx-auto rounded-lg w-10/12 bg-sky-500 m-3 px-3 py-1.5 hover:bg-sky-600 border-b-4 active:border-b-0 active:border-t-4 active:border-t-teal-800 border-sky-700 transition-all' onClick={e => {
+              e.preventDefault()
+              eventHandler(key)
+            }}>{key}</a>)}
+          </div>
+        </div>
       </div>
     </main>
   )
