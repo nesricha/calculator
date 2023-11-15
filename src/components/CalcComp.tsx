371f62c1f3a2bab253720c28c95dd2e53b008879
nesricha <nesrine2000@hotmail.it>
2023-11-15 18:33:31 +0100

    fixed n - n result

--- src/components/CalcComp.tsx
+++ src/components/CalcComp.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import InputComp from "./InputComp";
 import KeyboardComp from "./KeyboardComp";
 
@@ -32,7 +32,12 @@ export default function CalcComp() {
   const calcRes = () => {
     let expression = exp.join("");
     let result = new Function("return " + expression);
-    exp.length ? setRes(result()) : console.log("Invalid expression");
+    if (exp.length) {
+      setRes(result());
+      setExp([`${res}`]);
+    } else {
+      console.log("Invalid expression");
+    }
   };
 
   const eventHandler = (key: any) => {
@@ -74,10 +79,6 @@ export default function CalcComp() {
   //     ev.stopPropagation()
   //   })
   // }
-
-  useEffect(() => {
-    setExp([`${res}`]);
-  }, [res]);
 
   return (
     <div className="mx-auto w-4/12 min-w-[300px] max-w-[400px] bg-teal-800 border-b-8 border-b-teal-900 border-x-4 border-x-teal-950 text-white rounded-2xl p-2">
